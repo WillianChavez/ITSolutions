aa47bd95160c4c25c45144048ee7dc3afa839f1f
WillianChavez <williamchavezamaya@gmail.com>
2021-12-22 16:13:41 -0600

    fix errors for nav

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,9 +1,21 @@
 const navChk = document.getElementById('navChk')
+const nav = document.getElementById('nav')
+const navbarItems = document.querySelectorAll('.nav-bar__item')
 
 navChk.addEventListener('change', () => {
     if (navChk.checked) {
         document.body.style.overflowY = 'hidden'
+        nav.style.transform = 'translateX(0)'
     } else {
         document.body.style.overflowY = 'visible'
     }
 })
+
+navbarItems.forEach((item) => {
+    item.addEventListener('click', () => {
+        nav.style.transform = 'translateX(100%)'
+        document.body.style.overflowY = 'visible'
+        navChk.checked = false
+    })
+})
+console.log(navbarItems)
